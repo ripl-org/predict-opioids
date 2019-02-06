caf67169ada1d52c2c9686bef0d022b2ac604a0c
Mark Howison <mhowison@brown.edu>
2019-02-06 13:32:35 -0500

    [POPIOID-110] added feature for size of DHS household

--- source/features/Mega/household.py
+++ source/features/Mega/household.py
@@ -1,22 +1,23 @@
-import cx_Oracle
 import pandas as pd
 import os, sys, time
-from riipl import CachePopulation, SaveFeatures
+from riipl import CachePopulation, Connection, SaveFeatures
 
 population, lookback, mega, out, manifest = sys.argv[1:]
 
 def main():
     """
-    Indicator for births in the DHS household from MEGA table
+    Size of DHS household and indicator for births in the DHS household from MEGA table
     """
 
     index = ["RIIPL_ID"]
     features = CachePopulation(population, index).set_index(index)
 
     sql = """
-          SELECT DISTINCT
-                 pop.riipl_id,
-                 1 AS dhs_hh_birth
+          SELECT pop.riipl_id,
+                 COUNT(DISTINCT m2.riipl_id) AS dhs_hh_size,
+                 MAX(CASE WHEN m1.age > 1 AND m2.age < 1
+                          THEN 1
+                          ELSE 0 END)        AS dhs_hh_birth
             FROM {population} pop
        LEFT JOIN {lookback} lb
               ON pop.riipl_id = lb.riipl_id
@@ -25,22 +26,21 @@ def main():
                  lb.yrmo = m1.month
       INNER JOIN {mega} m2
               ON m1.dhs_hh_id = m2.dhs_hh_id AND
-                 m1.month = m2.month AND
-                 m2.age < 1
-           WHERE m1.dhs_hh_id IS NOT NULL AND
-                 m1.age > 1
+                 m1.month = m2.month
+        GROUP BY pop.riipl_id
           """.format(**globals())
 
-    with cx_Oracle.connect("/") as cxn:
-        features = features.join(pd.read_sql(sql, cxn).set_index(index))
+    with Connection() as cxn:
+        features = features.join(pd.read_sql(sql, cxn._connection).set_index(index))
 
     features = features.fillna(0)
 
     labels = {
+        "DHS_HH_SIZE"  : "count of distinct individuals in the DHS household during lookback period",
         "DHS_HH_BIRTH" : "a birth occurred in the DHS household during lookback period"
-        }
+    }
 
-    SaveFeatures(features, out, manifest, population, labels, bool_features=list(labels))
+    SaveFeatures(features, out, manifest, population, labels, bool_features=["DHS_HH_BIRTH"])
 
 
 # EXECUTE
--- source/models/bagged-lasso.R
+++ source/models/bagged-lasso.R
@@ -6,18 +6,18 @@ args <- commandArgs(trailingOnly=TRUE)
 
 model_file  <- args[1]
 model_files <- args[2:length(args)-1]
-out_file    <- args[lengths(args)]
+out_file    <- args[length(args)]
 
 load(model_file)
-y_test <- as.factor(model$y_test)
-y_predicted <- model$y_predicted
+y_pred_avg <- y_predicted
 
 for (model_file in model_files) {
     load(model_file)
-    y_predicted <- y_predicted + model$y_predicted
+    y_pred_avg <- y_pred_avg + y_predicted
 }
 
-y_predicted <- y_predicted / (length(model_files) + 1)
+y_pred_avg <- y_pred_avg / (length(model_files) + 1)
+y_test <- as.factor(y_test)
 
-print(paste0("auc: ", auc(roc(y_predicted, y_test))))
-write.csv(y_predicted, file=out_file)
+print(paste0("auc: ", auc(roc(y_pred_avg, y_test))))
+write.csv(y_pred_avg, file=out_file)
