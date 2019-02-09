493636e173b482b0511f4ab846d4ed0aa9b4ad8a
Mark Howison <mhowison@brown.edu>
2019-02-08 21:54:51 -0500

    [POPIOID-89] updated gbm model to use gbm.more

--- source/models/gbm.R
+++ source/models/gbm.R
@@ -19,35 +19,50 @@ print(selected)
 X_train <- X_train[,selected]
 X_validate <- X_validate[,selected]
 
-y_train <- as.factor(y_train[,c(outcome_name)])
+y_train <- y_train[,c(outcome_name)]
 y_validate <- as.factor(y_validate[,c(outcome_name)])
 
-best_auc <- 0
-tuned_auc <- 0
-test_auc <- 0
-delta <- 0.001
-trees <- 0
+trees <- 100
 depth <- 2
 shrinkages <- c(0.1, 0.01, 0.001)
+models <- list()
 best_model <- 0
+tuned_auc <- 0
 
-while (abs(best_auc - tuned_auc) > delta) {
-    trees <- trees + 100
-    for (shrinkage in shrinkages) {
-        model <- gbm(X_train, y_train,
-                     distribution="bernoulli",
-                     n.trees=trees,
-                     interaction.depth=depth,
-                     shrinkage=shrinkage)
-        y_pred <- predict(model, newdata=X_validate, type="response")
+for (i in 1:length(models)) {
+    models[i] <- gbm.fit(X_train, y_train,
+                         distribution="bernoulli",
+                         n.trees=trees,
+                         interaction.depth=depth,
+                         shrinkage=shrinkages[i])
+    y_pred <- predict(model, newdata=X_validate, n.trees=trees, type="response")
+    test_auc <- auc(roc(y_pred, y_validate))
+    print(paste("trees:", trees, "shrinkage:", shrinkage, "auc:", test_auc))
+    if (test_auc > tuned_auc) {
+        tuned_auc <- test_auc
+        best_model <- i
+    }
+}
+print(paste("trees:", trees, "tuned auc:", tuned_auc))
+
+delta <- 0.001
+best_auc <- 0
+
+while (tuned_auc - best_auc > delta) {
+    best_auc <- tuned_auc
+    tuned_auc <- 0
+    for (i in 1:length(models)) {
+        models[i] <- gbm.more(gbm, n.new.trees=trees)
+        y_pred <- predict(model, newdata=X_validate, n.trees=trees, type="response")
         test_auc <- auc(roc(y_pred, y_validate))
         print(paste("trees:", trees, "shrinkage:", shrinkage, "auc:", test_auc))
         if (test_auc > tuned_auc) {
             tuned_auc <- test_auc
-            best_model <- model
+            best_model <- i
         }
     }
     print(paste("trees:", trees, "tuned auc:", tuned_auc))
 }
 
-save(best_model, best_auc, y_predicted, y_test, file=model_file)
+model <- models[[best_model]]
+save(model, best_auc, file=model_file)
