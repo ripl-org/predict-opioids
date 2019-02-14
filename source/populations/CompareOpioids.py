2bc80f81ffe4cfec6772451c7263b994c38da064
Mark Howison <mhowison@brown.edu>
2019-02-14 10:36:59 -0500

    [POPIOID-111] added NDC ingredients to comparison

--- source/populations/CompareOpioids.py
+++ source/populations/CompareOpioids.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import os, sys, time
 
-ashp_path, ndc_path, out = sys.argv[1:]
+ashp_path, ndc_opioids_path, ndc_ing_path, out = sys.argv[1:]
 
 def main():
     ashp = pd.read_csv(ashp_path, usecols=["NDC9_CODE", "DESC"])
     opioids = frozenset(("Opiate Agonists", "Opiate Partial Agonists", "Opiate Antagonists"))
     ashp = ashp[ashp.DESC.isin(opioids)].drop_duplicates(["NDC9_CODE", "DESC"])
 
-    ndc = pd.read_csv("ndc_path").rename(columns={"ndc": "NDC9_CODE"})
+    ndc = pd.read_csv(ndc_opioids_path).rename(columns={"ndc": "NDC9_CODE"})
+    ing = pd.read_csv(ndc_ing_path).rename(columns={"ndc": "NDC9_CODE"})
 
     merged = ndc.merge(ashp, how="outer", on="NDC9_CODE")
 
-    merged[(merged.opioid != 1) & (merged.recovery != 1)].sort_values("NDC9_CODE").to_csv(out)
+    merged[(merged.opioid != 1) & (merged.recovery != 1) & merged.DESC.notnull()]\
+      .merge(ing, how="left", on="NDC9_CODE")\
+      .sort_values("NDC9_CODE")\
+      .to_csv(out, index=False, float_format="%g")
 
 # EXECUTE
 if __name__ == "__main__":
