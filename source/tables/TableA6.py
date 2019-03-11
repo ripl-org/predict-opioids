1eb8730eef319313fd1a166d96dfdc573be97ab6
Mark Howison <mhowison@brown.edu>
2019-03-11 12:54:00 -0400

    [POPIOID-114] merged Tables A6 and A7

--- source/tables/TableA6.py
+++ source/tables/TableA6.py
@@ -2,16 +2,22 @@ import pandas as pd
 import os
 import sys
 
-infile, outfile = sys.argv[1:]
+freq_file, odds_file, out_file = sys.argv[1:]
 
-table = pd.read_csv(infile)
-table = table[table.freq > 90]
+freq = pd.read_csv(freq_file)
+freq = freq[freq.freq > 90]
 
-with open(outfile, "w") as f:
-    print(r"\begin{tabular}{llr}", file=f)
-    print(r"\em Variable & \em Description & \em Frequency \\[0.5em]", file=f)
+odds = pd.read_csv(odds_file).sort_values("odds", ascending=False)
+
+table = freq.merge(odds, on="var")
+
+with open(out_file, "w") as f:
+    print(r"\begin{tabular}{lcccc}", file=f)
+    print(r"\em Variable & \em Odds Ratio & \em 95\% C.I. & \em p-value & \em Bootstrap Frequency\\[0.5em]", file=f)
     for row in table.itertuples():
-        print(r"{} & {} & {}\% \\".format(row.var.replace("_", r"\_"), row.desc, row.freq), file=f)
+        print(r"{} & {:.3f} & ({:.3f} - {:.3f}) & {:.3f} & {}\% \\".format(
+                  row.var.replace("_", r"\_"), row.odds, row.ci_lower, row.ci_upper, row.p, row.freq),
+              file=f)
     print(r"\end{tabular}", file=f)
 
 # vim: syntax=python expandtab sw=4 ts=4
--- source/tables/TableA7.py
+++ source/tables/TableA7.py
@@ -1,18 +1,47 @@
 import pandas as pd
 import os
 import sys
+from sklearn.metrics import roc_auc_score
 
-infile, outfile = sys.argv[1:]
+seed        = int(sys.argv[1])
+n_bootstrap = int(sys.argv[2])
+outcomes    = sys.argv[3].split(",")
+tensors     = sys.argv[4].split(",")
+infiles     = sys.argv[5:-1]
+outfile     = sys.argv[-1]
 
-table = pd.read_csv(infile).sort_values("odds", ascending=False)
+def bootstrap(data, N, statistic, seed):
+    replicates = sorted(statistic(data.sample(n=len(data),
+                                              replace=True,
+                                              random_state=seed+i))
+                        for i in range(N))
+    ci_lower = replicates[int(0.025 * N)]
+    ci_upper = replicates[int(0.975 * N)]
+    return statistic(data), ci_lower, ci_upper
 
+cells = []
+for y_pred_file in infiles:
+    auc, ci_lower, ci_upper = bootstrap(pd.read_csv(y_pred_file),
+                                        n_bootstrap,
+                                        lambda df: roc_auc_score(df.y_test, df.y_pred),
+                                        seed)
+    cells.append("{:.3f} ({:.3f}-{:.3f})".format(auc, ci_lower, ci_upper))
+
+labels = {
+    "OUTCOME_ANY": "Any",
+    "OUTCOME_DEPENDENCE": "Dependence",
+    "OUTCOME_ABUSE": "Abuse",
+    "OUTCOME_POISONING_RX": "Prescription-Opioid Poisoning",
+    "OUTCOME_POISONING_HEROIN": "Heroin Poisoning",
+    "OUTCOME_PROCEDURE": "Treatment"
+}
+
+N = len(outcomes)
 with open(outfile, "w") as f:
-    print(r"\begin{tabular}{lccc}", file=f)
-    print(r"\em Variable & \em Odds Ratio & \em 95\% C.I. & \em p-value\\[0.5em]", file=f)
-    for row in table.itertuples():
-        print(r"{} & {:.3f} & ({:.3f} - {:.3f}) & {:.3f} \\".format(
-                  row.var.replace("_", r"\_"), row.odds, row.ci_lower, row.ci_upper, row.p),
-              file=f)
+    print(r"\begin{tabular}{lllllll}", file=f)
+    print(r" & \em", r" & \em ".join(map(labels.get, outcomes)), r"\\[0.5em]", file=f)
+    for i, tensor in enumerate(tensors):
+        print(r"\textbf{" + tensor.replace("all", "Integrated") + "} &", " & ".join(cells[i*N:(i+1)*N]), r"\\", file=f)
     print(r"\end{tabular}", file=f)
 
 # vim: syntax=python expandtab sw=4 ts=4
