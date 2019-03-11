d01ad88e8da96e14904f81e3aa1fa83bcf4c671b
Mark Howison <mhowison@brown.edu>
2019-03-11 14:33:05 -0400

    [POPIOID-114] fixed sort order in Table A6

--- source/tables/TableA6.py
+++ source/tables/TableA6.py
@@ -7,16 +7,16 @@ freq_file, odds_file, out_file = sys.argv[1:]
 freq = pd.read_csv(freq_file)
 freq = freq[freq.freq > 90]
 
-odds = pd.read_csv(odds_file).sort_values("odds", ascending=False)
+odds = pd.read_csv(odds_file)
 
-table = freq.merge(odds, on="var")
+table = freq.merge(odds, on="var").sort_values("odds", ascending=False)
 
 with open(out_file, "w") as f:
     print(r"\begin{tabular}{lcccc}", file=f)
     print(r"\em Variable & \em Odds Ratio & \em 95\% C.I. & \em p-value & \em Bootstrap Frequency\\[0.5em]", file=f)
     for row in table.itertuples():
         if row.var.endswith("MISSING"):
-            desc = "{} is missing".format(row.var.partition("_")[0])
+            desc = "{} is missing".format(row.var.partition("_")[0].title())
         else:
             desc = row.desc
         print(r"{} & {:.3f} & ({:.3f} - {:.3f}) & {:.3f} & {}\% \\".format(
