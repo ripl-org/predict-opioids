941f5e6a6332ca76595315fe3287fb8ebbd7cd0a
Mark Howison <mhowison@ripl.org>
2019-07-24 15:31:06 -0400

    [POPIOID-123] fixes to Figure 3

--- source/figures/Figure3.R
+++ source/figures/Figure3.R
@@ -22,7 +22,7 @@ grid.arrange(csv %>% filter(Race=="All") %>%
                      theme_classic() +
                      scale_x_continuous(limits=c(1, 10), breaks=seq(1, 10)) +
                      scale_y_continuous(limits=c(0, 0.3), breaks=seq(0, 0.3, 0.05),
-					sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 135000, 22500), labels=dollar_format())),
+                                        sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 135000, 22500), labels=dollar_format())),
              csv %>% filter(Race!="All") %>%
                      ggplot(aes(x=Decile, y=CostRatio, color=Race)) +
                      ggtitle("b") +
@@ -33,7 +33,7 @@ grid.arrange(csv %>% filter(Race=="All") %>%
                      theme(legend.position=c(0.8, 0.8), plot.title=element_text(face="bold")) +
                      scale_x_continuous(limits=c(1, 10), breaks=seq(1, 10)) +
                      scale_y_continuous(limits=c(0, 0.5), breaks=seq(0, 0.5, 0.1),
-					sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 225000, 45000), labels=dollar_format())),
+                                        sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 225000, 45000), labels=dollar_format())) +
                      scale_color_brewer("Race/ethnicity", labels=c("Black"="African-American"), palette="Set2") +
                      scale_fill_brewer("Race/ethnicity", labels=c("Black"="African-American"), palette="Set2"),
              nrow=2)
