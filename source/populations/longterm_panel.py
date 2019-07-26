4fa983b9f65258fa356090c74e3e05e1c2f3ccb8
Mark Howison <mark@howison.org>
2019-07-26 17:03:25 -0400

    [POPIOID-126] added panel of longterm enrollees

---
+++ source/populations/longterm_panel.py
@@ -0,0 +1,35 @@
+import sys
+from riipl import Connection
+
+medicaid_enrollment, table = sys.argv[1:]
+
+sql = """
+      CREATE TABLE %table% NOLOGGING PCTFREE 0 PARALLEL AS 
+      SELECT riipl_id,
+             SUM(n) AS months
+        FROM (
+              SELECT riipl_id,
+                     yr,
+                     COUNT(yrmo) AS n
+                FROM (
+                      SELECT DISTINCT
+                             riipl_id,
+                             TO_NUMBER(SUBSTR(yrmo, 1, 4)) AS yr,
+                             TO_NUMBER(yrmo) AS yrmo
+                        FROM %medicaid_enrollment%
+                     )
+               WHERE yr BETWEEN 2008 AND 2012
+            GROUP BY riipl_id, yr
+              )
+        WHERE n >= 6              
+     GROUP BY riipl_id
+       HAVING COUNT(*) = 5
+     ORDER BY riipl_id
+      """
+
+with Connection() as cxn:
+    cxn.clear_tables(table)
+    cxn.execute(sql)
+    cxn.save_table(table, "RIIPL_ID")
+
+# vim: syntax=python expandtab sw=4 ts=4
