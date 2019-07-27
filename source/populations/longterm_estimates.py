4aae184adb9e4934b4876c385612a14487467756
Mark Howison <mark@howison.org>
2019-07-26 22:49:35 -0400

    [POPIOID-126] additional longterm estimates

--- source/populations/longterm_estimates.py
+++ source/populations/longterm_estimates.py
@@ -20,14 +20,24 @@ panel = panel.merge(pd.read_csv(rx_file, usecols=["RIIPL_ID", "INITIAL_RX_DT"]),
 with open(out_file, "w") as f:
 
     print("panel size:", len(panel), file=f)
-    print("median months enrolled:", panel.MONTHS.median(), file=f)
+    print("months enrolled:", file=f)
+    print(panel.MONTHS.describe(), file=f)
 
     adverse = panel.OUTCOME_ANY.fillna(MAX_DT) < MAX_DT
 
     n_adverse = adverse.sum()
-    print("adverse outcomes:", n_adverse, file=f)
+    print("adverse outcomes: {} ({:.1f}%)".format(n_adverse, 100.0 * n_adverse / len(panel)), file=f)
 
     n_without_rx = panel[adverse].INITIAL_RX_DT.isnull().sum()
-    print("adverse outcomes without rx: {} ({})".format(n_without_rx, 100.0 * n_without_rx / n_adverse), file=f)
+    print("adverse outcomes without rx: {} ({:.1f}%)".format(n_without_rx, 100.0 * n_without_rx / n_adverse), file=f)
+
+    rx = panel.INITIAL_RX_DT.notnull().sum()
+
+    n_rx = rx.sum()
+    print("rx: {} ({:.1f}%)".format(n_rx, 100.0 * n_rx / len(panel)), file=f)
+
+    n_without_adverse = (panel[rx].OUTCOME_ANY.fillna(MAX_DT) < MAX_DT).sum()
+    print("rx without adverse outcomes: {} ({:.1f}%)".format(n_without_adverse, 100.0 * n_without_adverse / n_rx), file=f)
+
 
 # vim: syntax=python expandtab sw=4 ts=4
