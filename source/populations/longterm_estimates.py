629480c0f81c4dd58d0752df4c549458dc5b002d
Mark Howison <mark@howison.org>
2019-07-27 21:32:30 -0400

    [POPIOID-127] began descriptive table for longterm panel

--- source/populations/longterm_estimates.py
+++ source/populations/longterm_estimates.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 from riipl import Connection
 
-panel, outcomes_file, rx_file, out_file = sys.argv[1:]
+panel, outcomes_file, rx_file, proc_file, out_file, csv_file = sys.argv[1:]
 
 MAX_DT = 99999999
 
@@ -14,6 +14,10 @@ panel = panel.merge(pd.read_csv(outcomes_file, usecols=["RIIPL_ID", "OUTCOME_ANY
                     on="RIIPL_ID")
 
 panel = panel.merge(pd.read_csv(rx_file, usecols=["RIIPL_ID", "INITIAL_RX_DT"]),
+                    how="left",
+                    on="RIIPL_ID")
+
+panel = panel.merge(pd.read_csv(proc_file, usecols=["RIIPL_ID", "OPIOID_PROC"]),
                     how="left",
                     on="RIIPL_ID")
 
@@ -41,5 +45,34 @@ with open(out_file, "w") as f:
     n_without_adverse = (panel[rx].OUTCOME_ANY.fillna(MAX_DT) == MAX_DT).sum()
     print("rx without adverse outcomes: {} ({:.1f}%)".format(n_without_adverse, 100.0 * n_without_adverse / n_rx), file=f)
 
+with open(csv_file, "w") as f:
+
+    proc = panel.OPIOID_PROC > 0
+    both = rx & proc
+    none = ~both
+
+    perc = lambda val, n: "{} ({}%)".format(val, 100.0*val/n)
+
+    subsets = [rx, proc, both, none]
+
+    outcomes = {
+        "OUTCOME_DEPENDENCE": "Dependence",
+        "OUTCOME_ABUSE": "Abuse",
+        "OUTCOME_POISONING_RX": "Rx Poisoning",
+        "OUTCOME_POISONING_HEROIN": "Heroin Poisoning",
+        "OUTCOME_PROCEDURE": "Treatment"
+    }
+    for outcome in outcomes:
+        panel[outcome] = panel[outcome] < MAX_DT
+
+    print("", "All", "Opioid Rx", "Opioid Injection", "Both", "Neither", sep=",", file="f")
+    print("N", len(panel), *[s.sum() for s in subsets], sep=",", file="f")
+
+    for outcome, desc in outcomes.items():
+        print(desc,
+              perc(panel[outcome].sum(), len(panel)),
+              *[perc(panel[s, outcome].sum(), s.sum()) for s in subsets],
+              sep=",",
+              file="f")
 
 # vim: syntax=python expandtab sw=4 ts=4
