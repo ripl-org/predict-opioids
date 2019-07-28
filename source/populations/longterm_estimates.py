07be3e01fa832143a8ba4ae73069837eccef2d7b
Mark Howison <mark@howison.org>
2019-07-27 21:43:40 -0400

    [POPIOID-127] fixes to descriptive table

--- source/populations/longterm_estimates.py
+++ source/populations/longterm_estimates.py
@@ -9,7 +9,7 @@ MAX_DT = 99999999
 with Connection() as cxn:
     panel = pd.read_sql("SELECT * FROM {}".format(panel), cxn._connection)
 
-panel = panel.merge(pd.read_csv(outcomes_file, usecols=["RIIPL_ID", "OUTCOME_ANY"]),
+panel = panel.merge(pd.read_csv(outcomes_file),
                     how="left",
                     on="RIIPL_ID")
 
@@ -51,7 +51,7 @@ with open(csv_file, "w") as f:
     both = rx & proc
     none = ~both
 
-    perc = lambda val, n: "{} ({}%)".format(val, 100.0*val/n)
+    perc = lambda val, n: "{} ({:.1f}%)".format(val, 100.0*val/n)
 
     subsets = [rx, proc, both, none]
 
@@ -65,14 +65,35 @@ with open(csv_file, "w") as f:
     for outcome in outcomes:
         panel[outcome] = panel[outcome] < MAX_DT
 
-    print("", "All", "Opioid Rx", "Opioid Injection", "Both", "Neither", sep=",", file="f")
-    print("N", len(panel), *[s.sum() for s in subsets], sep=",", file="f")
+    print("", "All", "Opioid Rx", "Opioid Injection", "Both", "Neither", sep=",", file=f)
+    print("N", len(panel), *[s.sum() for s in subsets], sep=",", file=f)
 
     for outcome, desc in outcomes.items():
         print(desc,
               perc(panel[outcome].sum(), len(panel)),
-              *[perc(panel[s, outcome].sum(), s.sum()) for s in subsets],
+              *[perc(panel.loc[s, outcome].sum(), s.sum()) for s in subsets],
               sep=",",
-              file="f")
+              file=f)
+
+    for q in [0.25, 0.5, 0.75]:
+        print("Age ({:.0f}th Percentile)".format(100*q),
+              "{:.1f}".format(panel["AGE"].quantile(q)),
+              *["{:.1f}".format(panel.loc[s, "AGE"].quantile(q)) for s in subsets],
+              sep=",",
+              file=f)
+
+    for race in ["White", "Black", "Hispanic"]:
+        print(race,
+              perc((panel["RACE"] == race).sum(), len(panel)),
+              *[perc((panel.loc[s, "RACE"] == race).sum(), s.sum()) for s in subsets],
+              sep=",",
+              file=f)
+
+    print("BMI (Median)",
+          "{:.1f}".format(panel["BMI"].quantile(0.5)),
+          *["{:.1f}".format(panel.loc[s, "BMI"].quantile(0.5)) for s in subsets],
+          sep=",",
+          file=f)
+
 
 # vim: syntax=python expandtab sw=4 ts=4
