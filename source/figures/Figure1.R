3ee47c1b5118299de26eb3c7293876a721505d5f
Mark Howison <mhowison@ripl.org>
2019-07-29 15:54:02 -0400

    [POPIOID-123] updates to Figure 1

--- source/figures/Figure1.R
+++ source/figures/Figure1.R
@@ -13,7 +13,7 @@ cats_path <- args[4]
 pdf_path  <- args[5]
 
 coef <- read_csv(coef_path) %>%
-        left_join(read_csv(desc_path, col_names=c("var", "desc")), by="var") %>%
+        left_join(read_tsv(desc_path, col_names=c("var", "desc")), by="var") %>%
         mutate(category=factor(case_when(grepl("_X_", var)            ~ "Interaction term",
                                          startsWith(var, "MEDICAID")  ~ "Medicaid claims and enrollment",
                                          startsWith(var, "ASHP")      ~ "Medicaid claims and enrollment",
@@ -36,19 +36,28 @@ coef <- read_csv(coef_path) %>%
                                          TRUE                         ~ "Demographics")))
 
 cats <- select(coef, "var", "category", "desc")
-write_csv(cats[order(cats$category, cats$var), :], cats_path)
+write_csv(cats[order(cats$category, cats$var),], cats_path)
 
 # Random jitter for x axis, proportional to distance from odds ratio of 1
-coef$jitter <- runif(rows(coef), min=-0.9, max=0.9) * (1 - abs(coef$odds - 1))^2
+coef$jitter <- runif(nrow(coef), min=-0.9, max=0.9) * (1 - abs(coef$odds - 1))^2
 
-pdf(pdf_path, width=6, height=10)
+pdf(pdf_path, width=3.4, height=5)
 coef %>% ggplot(aes(x=jitter, y=odds, color=category)) +
          geom_point(shape=1) +
          labs(y="Odds Ratio") +
-         scale_y_continuous(limits=c(0, 2), breaks=seq(0, 2, 0.2)) +
-         theme_classic() +
-         theme(legend.position=c(0.5, 0.5)) +
-         scale_color_brewer("Category", palette="Set2")
+         scale_x_continuous(limits=c(-1, 1), breaks=c()) +
+         scale_y_continuous(limits=c(0, 2.25), breaks=seq(0, 2.25, 0.25)) +
+         theme(
+               axis.text.x=element_blank(),
+               axis.ticks.x=element_blank(),
+               axis.title.x=element_blank(),
+               axis.text.y=element_text(size=5),
+               axis.ticks.y=element_blank(),
+               legend.position="right",
+               legend.text=element_text(size=7),
+               panel.border=element_blank(),
+               panel.grid.minor=element_blank()) +
+         scale_color_brewer("", palette="Set2")
 dev.off()
 
 # vim: syntax=R expandtab sw=4 ts=4
