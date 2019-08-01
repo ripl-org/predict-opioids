a37cb8f991cac07a4f924200aae109bc93e83b15
Mark Howison <mhowison@ripl.org>
2019-08-01 12:09:26 -0400

    [POPIOID-126] added cost ratio plot comparing alpha of 1.0 and 0.8

---
+++ source/figures/Figure2.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(readr)
+library(reshape2)
+library(scales)
+
+args <- commandArgs(trailingOnly=TRUE)
+
+csv_path <- args[1]
+out_path <- args[2]
+
+csv <- read_csv(csv_path)
+csv$Decile <- csv$Decile * 0.1
+
+pdf(out_path, width=6, height=4)
+csv %>% ggplot(aes(x=Decile, y=CostRatio, color=Alpha)) +
+        ggtitle(title) +
+        geom_ribbon(aes(ymin=CostRatioLower, ymax=CostRatioUpper, fill=Alpha), alpha=0.25, color=NA) +
+        geom_point(shape=1) +
+        labs(x="Cumulative Deciles by Decreasing Risk", y="Break-even Cost Ratio") +
+        theme_classic() +
+        theme(legend.position=c(0.8, 0.8), plot.title=element_text(face="bold")) +
+        scale_x_continuous(limits=c(0.1, 1), breaks=seq(0.1, 1, 0.1), labels=percent) +
+        scale_y_continuous(limits=c(0, 0.5), breaks=seq(0, 0.5, 0.1),
+                           sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 225000, 45000), labels=dollar_format())) +
+        scale_color_brewer("alpha", palette="Set2") +
+        scale_fill_brewer("alpha", palette="Set2")
+dev.off()
+
---
+++ source/figures/Figure2.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+import sys
+from functools import partial
+
+seed        = int(sys.argv[1])
+n_bootstrap = int(sys.argv[2])
+
+alphas = [1.0, 0.8]
+
+y_pred_file, demo_file, doc_file, med_file, out_file = sys.argv[3:]
+
+def cost_ratio(alpha, df):
+    tp = alpha * (df.y_test == 1).sum()
+    fp = (df.y_test == 0).sum()
+    return tp/(fp+tp)
+
+y = pd.read_csv(y_pred_file).sort_values("y_pred", ascending=False)
+
+decile = len(y) // 10
+
+replicates = [y.sample(n=len(y),
+                       replace=True,
+                       random_state=seed+i).sort_values("y_pred", ascending=False)
+              for i in range(n_bootstrap)]
+
+with open(out_file, "w") as f:
+
+    print("Decile", "Alpha", "CostRatio", "CostRatioLower", "CostRatioUpper", sep=",", file=f)
+
+    for i in range(1, 11):
+
+        yi = y.iloc[:i*decile]
+        ris = [r.iloc[:i*decile] for r in replicates]
+
+        # Recompute for each alpha
+        for alpha in alphas:
+            bootstraps = sorted(map(partial(cost_ratio, alpha), ris))
+            estimates = map("{:.3f}".format, [cost_ratio(alpha, yi),
+                                              bootstraps[int(0.025 * len(bootstraps))],
+                                              bootstraps[int(0.975 * len(bootstraps))]])
+            print(i, alpha, *estimates, sep=",", file=f)
+
+# vim: syntax=python expandtab sw=4 ts=4
