de248c876e5248872ed650945b44771439e7f7e9
Mark Howison <mark@howison.org>
2019-08-08 21:50:58 -0400

    [POPIOID-118] Sammy's edits to appendix

--- source/figures/FigureS3.R
+++ source/figures/FigureS3.R
@@ -21,7 +21,9 @@ plot <- function(df, title, grp_title, grp_labels) {
            geom_point() +
            labs(x="Cumulative Deciles by Decreasing Risk", y="Break-even Cost Ratio") +
            theme_classic() +
-           theme(legend.position=c(0.8, 0.8), plot.title=element_text(face="bold")) +
+           theme(legend.justification=c(1, 1),
+		 legend.position=c(0.99, 1),
+		 plot.title=element_text(face="bold")) +
            scale_x_continuous(limits=c(0.1, 1), breaks=seq(0.1, 1, 0.1), labels=percent_format(accuracy=1)) +
            scale_y_continuous(limits=c(0, 0.5), breaks=seq(0, 0.5, 0.1),
                               sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 225000, 45000), labels=dollar_format())) +
