d526ab61e864f7db9d90be7915975260e82f9a4a
Mark Howison <mhowison@ripl.org>
2019-11-06 13:27:26 -0500

    [POPIOID-136] reimplemented standardized differences

--- source/models/propensity-balance.R
+++ source/models/propensity-balance.R
@@ -1,3 +1,5 @@
+library(Matrix)
+
 args <- commandArgs(trailingOnly=TRUE)
 
 matrix_file <- args[1]
@@ -6,24 +8,23 @@ out_file    <- args[3]
 
 load(matrix_file, verbose=TRUE)
 injection <- which(colnames(X_train) == "INJECTION")
-y <- X_train[,injection]
-X <- X_train[,-injection]
-I <- (y == 1)
+I <- (X_train[,injection] == 1)
+X <- as.matrix(X_train[,-injection])
 
 # Calculate weights from propensity scores
 load(model_file, verbose=TRUE)
-w <- 1 / (1 - y_predicted)
-w[I] <- 1 / y_predicted[I]
+W <- 1 / (1 - y_predicted)
+W[I] <- 1 / y_predicted[I]
 
 # Unweighted standardized differences
-d <- (colMeans(X[I,] - colMeans(X[~I,]) / sqrt(0.5*(colVars(X[I,]) - colVars(X[~I,])))
+stdiff <- function(x) { (mean(x[I]) - mean(x[!I])) / sqrt(0.5*(var(x[I]) - var(x[!I]))) }
+d <- apply(X, 2, stdiff)
 
 # Weighted standardized differences
-xw1 <- w[ I] %*% X[ I,] / sum(w[ I])
-xw0 <- w[~I] %*% X[~I,] / sum(w[~I])
-vw1 <- sum(w[ I]) / (sum(w[ I])^2 - sum(w[ I]^2)) * (w[ I] %*% sweep(X[ I,], 2, xw1)^2)
-vw0 <- sum(w[~I]) / (sum(w[~I])^2 - sum(w[~I]^2)) * (w[~I] %*% sweep(X[~I,], 2, xw0)^2)
-dw <- (xw1 - xw0) / sqrt(0.5*(vw1 - vw2))
+wmean <- function(x, w) { sum(w * x) / sum(w) }
+wvar <- function(x, w) { sum(w) / (sum(w)^2 - sum(w^2)) * sum(w * (x - wmean(x, w))^2) }
+wstdiff <- function(x) { (wmean(x[I], W[I]) - wmean(x[!I], W[!I])) / sqrt(0.5*(wvar(x[I], W[I]) - wvar(x[!I], W[!I]))) }
+dw <- apply(X, 2, wstdiff)
 
 write.csv(data.frame(var=colnames(X),
                      d=d,
