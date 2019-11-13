caf39390857059bd8d7f7b85340ec180253da1f2
Mark Howison <mhowison@ripl.org>
2019-11-13 14:10:54 -0500

    [POPIOID-136] chi-squared test for stage 1

--- source/models/propensity-stage1.R
+++ source/models/propensity-stage1.R
@@ -13,35 +13,37 @@ coef_file     <- args[5]
 pred_file     <- args[6]
 
 load(matrix_file, verbose=TRUE)
-riipl_id <- y_train[,c("RIIPL_ID")]
-y_train <- as.vector(X_train[,c("INJECTION")])
 
 selected <- read.csv(selected_file, stringsAsFactors=FALSE)$var
 print(selected)
 
-X_train <- X_train[,selected]
-
-k <- kappa(X_train, exact=TRUE)
+X <- as.matrix(X_train[,selected])
+k <- kappa(X, exact=TRUE)
 print(paste("condition number (kappa):", k))
 assert_that(k < 100)
 
-# add intercept
-X_train <- cbind(X_train, 1)
+df <- as.dta.frame(X)
+df$INJECTION <- as.vector(X_train[,c("INJECTION")])
 
-model <- glm.fit(x=X_train, y=y_train, family=binomial())
-cat(summary.glm(model), file=summary_file)
-params <- summary.glm(model)$coefficients
+model <- glm(INJECTION ~ ., data=df, family=binomial)
+params <- summary(model)$coefficients
+
+sink(summary_file)
+print(summary(model))
+print("Chi-squared p-value:")
+print(pchisq(model$null.deviance - model$deviance, model$df.null - model$df.residual, lower.tail=FALSE))
+sink()
 
 # Convert coefficients to odds ratios and standard errors to 95% C.I.
 odds <- exp(params[,1])
 ci_lower <- exp(params[,1] - 1.96*params[,2])
 ci_upper <- exp(params[,1] + 1.96*params[,2])
-write.csv(data.frame(var=c(selected, "intercept"), odds=odds, ci_lower=ci_lower, ci_upper=ci_upper, p=params[,4]),
+write.csv(data.frame(var=rownames(params), odds=odds, ci_lower=ci_lower, ci_upper=ci_upper, p=params[,4]),
           file=coef_file, row.names=FALSE)
 
 # Predict on training data for propensity score
-coef <- as.matrix(model$coef)
-eta <- as.matrix(X_train) %*% as.matrix(coef)
-y_pred <- exp(eta)/(1 + exp(eta))
-write.csv(data.frame(RIIPL_ID=riipl_id, y_pred=y_pred, y_train=y_train), file=pred_file, row.names=FALSE)
-
+write.csv(data.frame(RIIPL_ID=y_train[,c("RIIPL_ID")],
+                     y_pred=predict(model, newdata=df, type="response"),
+                     y_train=df$INJECTION),
+          file=pred_file,
+          row.names=FALSE)
