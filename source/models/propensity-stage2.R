59aa9216e351ae2001e9c62c9002c92bbacc2221
Mark Howison <mark@howison.org>
2019-11-19 17:44:45 -0500

    [POPIOID-136] reimplemented stage 2, with Tables S14/S15

--- source/models/propensity-stage2.R
+++ source/models/propensity-stage2.R
@@ -1,45 +1,67 @@
-library(gamlr)
-library(PRROC)
-
-gammas <- c(0,1,10,100,1000)
+library(assertthat)
+library(Matrix)
+library(tidyverse)
 
 args <- commandArgs(trailingOnly=TRUE)
 
 set.seed(args[1])
 
-matrix_file   <- args[2]
-model_file    <- args[3]
-beta_file     <- args[4]
+matrix_file  <- args[2]
+coef_file    <- args[3]
+pred_file    <- args[4]
+summary_file <- args[5]
+coef1_file   <- args[6]
+coef2_file   <- args[7]
 
 load(matrix_file, verbose=TRUE)
 
-# Remove injection status from X
-injection  <- which(colnames(X_train) == "INJECTION")
-X_train    <- X_train[,-injection]
-X_validate <- X_validate[,-injection]
+# Create injection indicator
+I <- (X_train[,c("INJECTION")] == 1)
 
-# Grid search for model with best gamma and lambda
-models <- lapply(gammas, function(gamma) {
-  model <- gamlr(x=X_train, y=y_train, family="binomial", gamma=gamma, standardize=FALSE)
-  model$auprcs <- sapply(1:100, function(i) {
-    y_predicted <- predict(model, newdata=X_validate, type="response", select=i)
-    return(pr.curve(y_predicted, weights.class0=y_validate)$auc.integral)
-  })
-  model$auprc <- max(model$auprcs)
-  model$best_lambda <- which.max(model$auprcs)
-  return(model)
-})
+# Select variables from BOLASSO
+selected <- read_csv(coef_file) %>%
+            filter(var != "(Intercept)")
 
-best_model <- which.max(lapply(models, function(model) { model$auprc }))
-model <- models[[best_model]]
+# Add injection to selected variables
+selected <- c("INJECTION", selected)
+print(selected)
 
-best_gamma  <- gammas[best_model]
-print(paste0("best gamma: ", best_gamma))
+# Create dataframe
+df <- as.data.frame(as.matrix(X_train[,selected$var]))
+df$OUTCOME <- y_train[,c("OUTCOME_ANY")]
 
-best_lambda <- model$best_lambda
-print(paste0("best lambda: ", best_lambda))
+# Calculate weights from propensity scores
+y_pred <- read_csv(pred_file)$y_pred
+assert_that(all(y_pred <= 0.9))
+W <- case_when( I ~ 1,
+               !I ~ y_pred / (1 - y_pred))
 
-y_predicted <- predict(model, newdata=X_train, type="response", select=model$best_lambda)
+sink(summary_file)
 
-save(model, best_gamma, y_predicted, y_train, file=model_file)
-write.csv(model$beta[,best_lambda], file=beta_file)
+# Weighted regression against all selected variables
+model <- glm(OUTCOME ~ ., data=df, family=binomial, weights=W)
+print(summary(model))
+print("Chi-squared p-value:")
+print(pchisq(model$null.deviance - model$deviance, model$df.null - model$df.residual, lower.tail=FALSE))
+
+# Convert coefficients to odds ratios and standard errors to 95% C.I.
+params <- summary(model)$coefficients
+odds <- exp(params[,1])
+ci_lower <- exp(params[,1] - 1.96*params[,2])
+ci_upper <- exp(params[,1] + 1.96*params[,2])
+write.csv(data.frame(var=rownames(params), odds=odds, ci_lower=ci_lower, ci_upper=ci_upper, p=params[,4]),
+          file=coef1_file, row.names=FALSE)
+
+# Weighted regression against injection
+model <- glm(OUTCOME ~ INJECTION, data=df, family=binomial, weights=W)
+print(summary(model))
+
+# Convert coefficients to odds ratios and standard errors to 95% C.I.
+params <- summary(model)$coefficients
+odds <- exp(params[,1])
+ci_lower <- exp(params[,1] - 1.96*params[,2])
+ci_upper <- exp(params[,1] + 1.96*params[,2])
+write.csv(data.frame(var=rownames(params), odds=odds, ci_lower=ci_lower, ci_upper=ci_upper, p=params[,4]),
+          file=coef2_file, row.names=FALSE)
+
+sink()
---
+++ source/tables/TableS14.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import sys
+
+manifest_file, odds_file, out_file = sys.argv[1:]
+
+manifest = pd.read_csv(manifest_file, sep="\t", names=["var", "desc"])
+odds = pd.read_csv(odds_file)
+
+table = odds.merge(manifest, on="var", how="left").sort_values("odds", ascending=False)
+
+def significance(p):
+    if   p < 0.001 : return "$^{***}$"
+    elif p < 0.01  : return "$^{**}$"
+    elif p < 0.05  : return "$^{*}$"
+    else           : return ""
+
+with open(out_file, "w") as f:
+    print(r"\begin{tabular}{lccc}", file=f)
+    print(r"\em Variable & \em Odds Ratio & \em 95\% C.I. & \em p-value \\[0.5em]", file=f)
+    for row in table.itertuples():
+        if row.var.endswith("MISSING"):
+            desc = "{} is missing".format(row.var.partition("_")[0].title())
+        else:
+            desc = row.desc
+        print(r"{} & {:.3f} & ({:.3f} - {:.3f}) & {:.3f}{} \\".format(
+                  desc, row.odds, row.ci_lower, row.ci_upper, row.p, significance(row.p))
+              file=f)
+    print(r"\end{tabular}", file=f)
+
+# vim: syntax=python expandtab sw=4 ts=4
