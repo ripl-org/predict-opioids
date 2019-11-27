7c61b39a64653f656131d8c90ca2699f282de138
Mark Howison <mark@howison.org>
2019-11-27 00:54:00 -0500

    [POPIOID-135] added Section 8 to appendix on correlation

--- source/figures/FigureS3.R
+++ source/figures/FigureS3.R
@@ -18,9 +18,9 @@ csv %>% ggplot(aes(x=Decile, y=CostRatio, color=Rho)) +
         theme_classic() +
         theme(legend.position=c(0.8, 0.8), plot.title=element_text(face="bold")) +
         scale_x_continuous(limits=c(0.1, 1), breaks=seq(0.1, 1, 0.1), labels=percent_format(accuracy=1)) +
-        scale_y_continuous(limits=c(0, 0.5), breaks=seq(0, 0.5, 0.1),
+        scale_y_continuous(limits=c(-0.05, 0.5), breaks=seq(0, 0.5, 0.1),
                            sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 225000, 45000), labels=dollar_format())) +
         scale_color_brewer(expression(rho), labels=c("1"="1.00", "0.5"="0.50", "0"="0.00"), palette="Set2") +
-        scale_fill_brewer(expression(rho), labels=c("1"="1.00", "0.5"="0.50", "0=0.00"), palette="Set2")
+        scale_fill_brewer(expression(rho), labels=c("1"="1.00", "0.5"="0.50", "0"="0.00"), palette="Set2")
 dev.off()
 
