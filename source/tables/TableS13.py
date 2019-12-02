3d076e0c76e6231e171a6791c9f917e845e6ed1c
Mark Howison <mhowison@ripl.org>
2019-12-02 15:07:21 -0500

    [POPIOID-135] updated table numbering

--- source/tables/TableS13.py
+++ source/tables/TableS13.py
@@ -1,36 +1,47 @@
 import pandas as pd
 import os
 import sys
+from sklearn.metrics import roc_auc_score
 
-manifest_file, odds_file, out_file = sys.argv[1:]
+seed        = int(sys.argv[1])
+n_bootstrap = int(sys.argv[2])
+outcomes    = sys.argv[3].split(",")
+tensors     = sys.argv[4].split(",")
+infiles     = sys.argv[5:-1]
+outfile     = sys.argv[-1]
 
-manifest = pd.read_csv(manifest_file, sep="\t", names=["var", "desc"])
-odds = pd.read_csv(odds_file)
+def bootstrap(data, N, statistic, seed):
+    replicates = sorted(statistic(data.sample(n=len(data),
+                                              replace=True,
+                                              random_state=seed+i))
+                        for i in range(N))
+    ci_lower = replicates[int(0.025 * N)]
+    ci_upper = replicates[int(0.975 * N)]
+    return statistic(data), ci_lower, ci_upper
 
-table = odds.merge(manifest, on="var", how="left").sort_values("odds", ascending=False)
+cells = []
+for y_pred_file in infiles:
+    auc, ci_lower, ci_upper = bootstrap(pd.read_csv(y_pred_file),
+                                        n_bootstrap,
+                                        lambda df: roc_auc_score(df.y_test, df.y_pred),
+                                        seed)
+    cells.append("{:.3f} ({:.3f}-{:.3f})".format(auc, ci_lower, ci_upper))
 
-def significance(p):
-    if   p < 0.001 : return "$^{***}$"
-    elif p < 0.01  : return "$^{**}$"
-    elif p < 0.05  : return "$^{*}$"
-    else           : return ""
+labels = {
+    "OUTCOME_ANY": "Any",
+    "OUTCOME_DEPENDENCE": "Dependence",
+    "OUTCOME_ABUSE": "Abuse",
+    "OUTCOME_POISONING_RX": "Prescription-Opioid Poisoning",
+    "OUTCOME_POISONING_HEROIN": "Heroin Poisoning",
+    "OUTCOME_PROCEDURE": "Treatment"
+}
 
-with open(out_file, "w") as f:
-    print(r"\begin{tabular}{lcccc}", file=f)
-    print(r" & \multicolumn{2}{c}{Unweighted} & \multicolumn{2}{c}{Weighted} \\", file=f)
-    print(r"\em Variable & \em Odds Ratio & \em p-value & \em Odds Ratio & \em p-value\\", file=f)
-    print(r" & \em (95\% C.I.) & & \em (95\% C.I.) & \\[0.5em]", file=f)
-    for row in table.itertuples():
-        if row.var.endswith("MISSING"):
-            desc = "{} is missing".format(row.var.partition("_")[0].title())
-        else:
-            desc = row.desc
-        print(r"{} & {:.3f} & {:.3f}{} & {:.3f} & {:.3f}{} \\".format(
-                  desc, row.odds, row.p, significance(row.p), row.w_odds, row.w_p, significance(row.w_p)),
-              file=f)
-        print(r" & ({:.3f} - {:.3f}) &  & ({:.3f} - {:.3f}) & \\".format(
-                  row.ci_lower, row.ci_upper, row.w_ci_lower, row.w_ci_upper),
-              file=f)
+N = len(outcomes)
+with open(outfile, "w") as f:
+    print(r"\begin{tabular}{lllllll}", file=f)
+    print(r" & \em", r" & \em ".join(map(labels.get, outcomes)), r"\\[0.5em]", file=f)
+    for i, tensor in enumerate(tensors):
+        print(r"\textbf{" + tensor.replace("all", "Integrated") + "} &", " & ".join(cells[i*N:(i+1)*N]), r"\\", file=f)
     print(r"\end{tabular}", file=f)
 
 # vim: syntax=python expandtab sw=4 ts=4
---
+++ source/tables/TableS15.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+import sys
+
+manifest_file, odds_file, out_file = sys.argv[1:]
+
+manifest = pd.read_csv(manifest_file, sep="\t", names=["var", "desc"])
+odds = pd.read_csv(odds_file)
+
+table = odds.merge(manifest, on="var", how="left").sort_values("odds", ascending=False)
+
+def significance(p):
+    if   p < 0.001 : return "$^{***}$"
+    elif p < 0.01  : return "$^{**}$"
+    elif p < 0.05  : return "$^{*}$"
+    else           : return ""
+
+with open(out_file, "w") as f:
+    print(r"\begin{tabular}{lcccc}", file=f)
+    print(r" & \multicolumn{2}{c}{Unweighted} & \multicolumn{2}{c}{Weighted} \\", file=f)
+    print(r"\em Variable & \em Odds Ratio & \em p-value & \em Odds Ratio & \em p-value\\", file=f)
+    print(r" & \em (95\% C.I.) & & \em (95\% C.I.) & \\[0.5em]", file=f)
+    for row in table.itertuples():
+        if row.var.endswith("MISSING"):
+            desc = "{} is missing".format(row.var.partition("_")[0].title())
+        else:
+            desc = row.desc
+        print(r"{} & {:.3f} & {:.3f}{} & {:.3f} & {:.3f}{} \\".format(
+                  desc, row.odds, row.p, significance(row.p), row.w_odds, row.w_p, significance(row.w_p)),
+              file=f)
+        print(r" & ({:.3f} - {:.3f}) &  & ({:.3f} - {:.3f}) & \\".format(
+                  row.ci_lower, row.ci_upper, row.w_ci_lower, row.w_ci_upper),
+              file=f)
+    print(r"\end{tabular}", file=f)
+
+# vim: syntax=python expandtab sw=4 ts=4
