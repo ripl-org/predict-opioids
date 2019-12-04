635bc7dfe72612128c1a73159e9139913b84ffd5
Mark Howison <mhowison@ripl.org>
2019-12-04 14:14:21 -0500

    [POPIOID-135] updates to appendix section 7

--- source/figures/FigureS3.R
+++ source/figures/FigureS3.R
@@ -8,7 +8,7 @@ out_path <- args[2]
 
 csv <- read_csv(csv_path)
 csv$Decile <- csv$Decile * 0.1
-csv$Rho <- factor(csv$Rho, c(0, 0.5, 1, 4.31))
+csv$Rho <- factor(csv$Rho, c(0, 0.5, 1, 4.02))
 
 pdf(out_path, width=6.8, height=3.4)
 csv %>% ggplot(aes(x=Decile, y=CostRatio, color=Rho)) +
@@ -17,7 +17,7 @@ csv %>% ggplot(aes(x=Decile, y=CostRatio, color=Rho)) +
         labs(x="Cumulative Deciles by Decreasing Risk", y="Break-even Cost Ratio") +
         theme_classic() +
         theme(legend.position=c(0.8, 0.8), plot.title=element_text(face="bold")) +
-        scale_x_continuous(limits=c(0.1, 1), breaks=seq(0.1, 1, 0.1), labels=percent_format(accuracy=1)) +
+        scale_x_continuous(limits=c(0.1, 1), breaks=seq(0.1, 1, 0.1), labels=percent_format()) +
         scale_y_continuous(limits=c(-0.05, 0.5), breaks=seq(0, 0.5, 0.1),
                            sec.axis=sec_axis(~ . * 450000, name="Break-even Diversion Cost", breaks=seq(0, 225000, 45000), labels=dollar_format())) +
         scale_color_brewer(expression(rho), labels=c("1"="1.00", "0.5"="0.50", "0"="0.00"), palette="Set2") +
--- source/figures/FigureS3.py
+++ source/figures/FigureS3.py
@@ -6,13 +6,13 @@ from functools import partial
 seed        = int(sys.argv[1])
 n_bootstrap = int(sys.argv[2])
 
-rhos = [0, 0.5, 1.0, 4.31]
+rhos = [0, 0.5, 1.0, 4.02]
 
 y_pred_file, out_file = sys.argv[3:]
 
 def cost_ratio(rho, df):
     tp = (df.y_test == 1).sum() / len(df)
-    tp = tp * (1 - rho * tp)
+    tp = tp * (1 - rho * (df.y_pred.sum() / len(df)))
     fp = (df.y_test == 0).sum() / len(df)
     return tp/(fp+tp)
 
@@ -27,7 +27,7 @@ replicates = [y.sample(n=len(y),
 
 with open(out_file, "w") as f:
 
-    print("Decile", "Rho", "TPR", "CostRatio", "CostRatioLower", "CostRatioUpper", sep=",", file=f)
+    print("Decile", "Rho", "Yhat", "TPR", "CostRatio", "CostRatioLower", "CostRatioUpper", sep=",", file=f)
 
     for i in range(1, 11):
 
@@ -37,7 +37,8 @@ with open(out_file, "w") as f:
         # Recompute for each rho
         for rho in rhos:
             bootstraps = sorted(map(partial(cost_ratio, rho), ris))
-            estimates = map("{:.3f}".format, [(yi.y_test == 1).sum() / len(yi),
+            estimates = map("{:.3f}".format, [yi.y_pred.sum() / len(yi),
+                                              (yi.y_test == 1).sum() / len(yi),
                                               cost_ratio(rho, yi),
                                               bootstraps[int(0.025 * len(bootstraps))],
                                               bootstraps[int(0.975 * len(bootstraps))]])
